93946212460d4588c8208cb06cc8f5b899538d69
Carden <carden.ruby@gmail.com>
2021-03-15 19:53:25 +0300

    complited

--- project_2/main.py
+++ project_2/main.py
@@ -1,6 +1,10 @@
+import random
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup
 import json
+import csv
 
 # url = "https://health-diet.ru/table_calorie/"
 #
@@ -41,28 +45,97 @@ headers = {
 with open("all_categories_dict.json") as file:
     all_categories = json.load(file)
 
+iteration_count = int(len(all_categories)) - 1
 count = 1
+print(f"Всего итераций: {iteration_count}")
+
 for category_name, category_href in all_categories.items():
+    rep = [",", " ", "-", "'"]
+    for item in rep:
+        if item in category_name:
+            category_name = category_name.replace(item, '_')
 
-    if count == 1:
-        rep = [",", " ", "-", "'"]
-        for item in rep:
-            if item in category_name:
-                category_name = category_name.replace(item, '_')
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
 
-        req = requests.get(url=category_href, headers=headers)
-        src = req.text
+    with open(f"data/{count}_{category_name}.html", "w") as file:
+        file.write(src)
 
-        with open(f"data/{count}_{category_name}.html", "w") as file:
-            file.write(src)
+    with open(f"data/{count}_{category_name}.html") as file:
+        src = file.read()
 
-        with open(f"data/{count}_{category_name}.html") as file:
-            src = file.read()
+    soup = BeautifulSoup(src, "lxml")
 
-        soup = BeautifulSoup(src, "lxml")
+    # Проверка страницы на наличие таблицы с продуктами
 
-        # Собираем заголовки таблицы
-        table_head = soup.find(calss_="mzr-tc-group-table")
-        print(table_head)
+    alert_block = soup.find(class_="uk-alert-danger")
+    if alert_block is not None:
+        continue
 
-        count += 1
+    # Собираем заголовки таблицы
+    table_head = soup.find(class_="mzr-tc-group-table").find("tr").find_all("th")
+    product = table_head[0].text
+    calories = table_head[1].text
+    proteins = table_head[2].text
+    fats = table_head[3].text
+    carbohydrates = table_head[4].text
+
+    with open(f"data/{count}_{category_name}.csv", "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                product,
+                calories,
+                proteins,
+                fats,
+                carbohydrates
+            )
+        )
+
+    # Собираем данные продуктов
+    products_data = soup.find(class_="mzr-tc-group-table").find("tbody").find_all("tr")
+
+    product_info = []
+    for item in products_data:
+        product_tds = item.find_all("td")
+
+        title = product_tds[0].find("a").text
+        calories = product_tds[1].text
+        proteins = product_tds[2].text
+        fats = product_tds[3].text
+        carbohydrates = product_tds[4].text
+
+        product_info.append(
+            {
+                "Title": title,
+                "Calories": calories,
+                "Proteins": proteins,
+                "Fats": fats,
+                "Carbohydrates": carbohydrates
+            }
+        )
+
+        with open(f"data/{count}_{category_name}.csv", "a", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (
+                    title,
+                    calories,
+                    proteins,
+                    fats,
+                    carbohydrates
+                )
+            )
+
+    with open(f"data/{count}_{category_name}.json", "a", encoding="utf-8") as file:
+        json.dump(product_info, file, indent=4, ensure_ascii=False)
+
+    print(f"# Итерация {count}. {category_name} записан...")
+    count += 1
+    iteration_count -= 1
+    if iteration_count == 0:
+        print("Работа завершена")
+        break
+
+    print(f"Осталось итераций {iteration_count}")
+    sleep(random.randrange(2, 4))
