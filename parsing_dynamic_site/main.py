2420724f122a9a9694ce6ca9078ea35807e35eee
Carden <carden.ruby@gmail.com>
2021-03-15 22:48:13 +0300

    completed

--- parsing_dynamic_site/main.py
+++ parsing_dynamic_site/main.py
@@ -1,4 +1,8 @@
 import json
+import os
+import random
+import re
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,76 +16,117 @@ def get_data(url):
                       "Safari/537.36 "
     }
 
-    # req = requests.get(url, headers=headers)
-    #
-    # with open("projects.html", "w") as file:
-    #     file.write(req.text)
-
-    with open("projects.html") as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, "lxml")
-    articles = soup.find_all("article", class_="ib19")
-
-    projects_urls = []
-    for article in articles:
-        project_url = "http://www.edutainme.ru" + article.find("div", class_="txtBlock").find("a").get("href")
-
-        projects_urls.append(project_url)
-
     projects_data_list = []
-    for project_url in projects_urls:
-        req = requests.get(project_url, headers)
-        project_name = project_url.split("/")[-2]
 
-        with open(f"data/{project_name}.html", "w") as file:
+    iteration_count = 23
+    print(f"Всего итераций: #{iteration_count}")
+
+    for item in range(1, 24):
+        req = requests.get(url + f"&PAGEN_1={item}&PAGEN_2={item}", headers=headers)
+
+        folder_name = f"data/data_{item}"
+
+        if os.path.exists(folder_name):
+            print("Папка уже существует!")
+        else:
+            os.mkdir(folder_name)
+
+        with open(f"{folder_name}/projects_{item}.html", "w") as file:
             file.write(req.text)
 
-        with open(f"data/{project_name}.html") as file:
+        with open(f"{folder_name}/projects_{item}.html") as file:
             src = file.read()
 
         soup = BeautifulSoup(src, "lxml")
+        articles = soup.find_all("article", class_="ib19")
 
-        project_data = soup.find("div", class_="inside")
+        projects_urls = []
+        for article in articles:
+            project_url = "http://www.edutainme.ru" + article.find("div", class_="txtBlock").find("a").get("href")
+            projects_urls.append(project_url)
 
-        try:
-            project_logo = "http://www.edutainme.ru" + project_data.find("div", class_="Img logo").find("img").get("src")
-        except Exception:
-            project_logo = "No project logo"
+        for project_url in projects_urls:
+            req = requests.get(project_url, headers)
+            project_name = project_url.split("/")[-2]
 
-        try:
-            project_name = project_data.find("div", class_="txt").find("h1").text
-        except Exception:
-            project_name = "No project name"
+            with open(f"{folder_name}/{project_name}.html", "w") as file:
+                file.write(req.text)
 
-        try:
-            project_short_description = project_data.find("div", class_="txt").find("h4", class_="head").text
-        except Exception:
-            project_short_description = "No project short description"
+            with open(f"{folder_name}/{project_name}.html") as file:
+                src = file.read()
 
-        try:
-            project_website = project_data.find("div", class_="txt").find("p").find("a").text
-        except Exception:
-            project_website = "No project site"
+            soup = BeautifulSoup(src, "lxml")
 
-        try:
-            project_full_description = project_data.find("div", class_="textWrap").find("div", class_="rBlock").text
-        except Exception:
-            project_full_description = "No full description"
+            project_data = soup.find("div", class_="inside")
 
-        projects_data_list.append(
-            {
-                "Имя проекта:": project_name,
-                "URL логотипа проекта:": project_logo,
-                "Короткое описание:": project_short_description,
-                "Сайт проекта:": project_website,
-                "Полное описание проекта:": project_full_description.strip()
-            }
-        )
+            try:
+                project_logo = "http://www.edutainme.ru" + project_data.find("div", class_="Img logo").find("img").get("src")
+            except Exception:
+                project_logo = "No project logo"
+
+            try:
+                project_name = project_data.find("div", class_="txt").find("h1").text
+            except Exception:
+                project_name = "No project name"
+
+            try:
+                project_short_description = project_data.find("div", class_="txt").find("h4", class_="head").text
+            except Exception:
+                project_short_description = "No project short description"
+
+            try:
+                project_website = project_data.find("div", class_="txt").find("p").find("a").text
+            except Exception:
+                project_website = "No project site"
+
+            try:
+                project_full_description = project_data.find("div", class_="textWrap").find("div", class_="rBlock").text
+            except Exception:
+                project_full_description = "No full description"
+
+            # Убирает все теги и переносы строк из строки.
+            def replace_string(string):
+                string = string.replace("\n", "")
+                return "".join(re.sub("<.*?>", "", string))
+
+            project_short_description = project_short_description.replace("  ", "")
+            project_full_description = replace_string(project_full_description)
+
+            projects_data_list.append(
+                {
+                    "Имя проекта:": project_name,
+                    "URL логотипа проекта:": project_logo,
+                    "Короткое описание:": project_short_description,
+                    "Сайт проекта:": project_website,
+                    "Полное описание проекта:": project_full_description.strip()
+                }
+            )
+        iteration_count -= 1
+        print(f"Итерация: #{item} завершена, осталось итераций: {iteration_count}")
+        if iteration_count == 0:
+            print("Сбор данных завершён.")
+        time.sleep(random.randrange(2, 4))
 
     with open(f"data/projects_data.json", "a", encoding="utf-8") as file:
         json.dump(projects_data_list, file, indent=4, ensure_ascii=False)
 
 
+def main():
+    get_data("http://www.edutainme.ru/edindex/ajax.php?params=%7B%22LETTER%22%3Anull%2C%22RESTART%22%3A%22N%22%2C"
+             "%22CHECK_DATES%22%3Afalse%2C%22arrWHERE%22%3A%5B%22iblock_startaps%22%5D%2C%22arrFILTER%22%3A%5B"
+             "%22iblock_startaps%22%5D%2C%22startups%22%3A%22Y%22%2C%22SHOW_WHERE%22%3Atrue%2C%22PAGE_RESULT_COUNT%22%3A9"
+             "%2C%22CACHE_TYPE%22%3A%22A%22%2C%22CACHE_TIME%22%3A0%2C%22TAGS_SORT%22%3A%22NAME%22%2C%22TAGS_PAGE_ELEMENTS"
+             "%22%3A%22999999999999999999%22%2C%22TAGS_PERIOD%22%3A%22%22%2C%22TAGS_URL_SEARCH%22%3A%22%22%2C"
+             "%22TAGS_INHERIT%22%3A%22Y%22%2C%22SHOW_RATING%22%3A%22Y%22%2C%22FONT_MAX%22%3A%2214%22%2C%22FONT_MIN%22%3A"
+             "%2214%22%2C%22COLOR_NEW%22%3A%22000000%22%2C%22COLOR_OLD%22%3A%22C8C8C8%22%2C%22PERIOD_NEW_TAGS%22%3A%22%22"
+             "%2C%22DISPLAY_TOP_PAGER%22%3A%22N%22%2C%22DISPLAY_BOTTOM_PAGER%22%3A%22N%22%2C%22SHOW_CHAIN%22%3A%22Y%22%2C"
+             "%22COLOR_TYPE%22%3A%22Y%22%2C%22WIDTH%22%3A%22100%25%22%2C%22USE_LANGUAGE_GUESS%22%3A%22N%22%2C"
+             "%22PATH_TO_USER_PROFILE%22%3A%22%23SITE_DIR%23people%5C%2Fuser%5C%2F%23USER_ID%23%5C%2F%22%2C%22SHOW_WHEN"
+             "%22%3Afalse%2C%22PAGER_TITLE%22%3A%22%5Cu0420%5Cu0435%5Cu0437%5Cu0443%5Cu043b%5Cu044c%5Cu0442%5Cu0430"
+             "%5Cu0442%5Cu044b+%5Cu043f%5Cu043e%5Cu0438%5Cu0441%5Cu043a%5Cu0430%22%2C%22PAGER_SHOW_ALWAYS%22%3Atrue%2C"
+             "%22USE_TITLE_RANK%22%3Afalse%2C%22PAGER_TEMPLATE%22%3A%22%22%2C%22DEFAULT_SORT%22%3A%22rank%22%2C%22noTitle"
+             "%22%3A%22Y%22%7D")
 
-get_data("http://www.edutainme.ru/edindex/")
+
+if __name__ == '__main__':
+    main()
--- parsing_dynamic_site/test.py
+++
@@ -1,3 +0,0 @@
-url = "http://www.edutainme.ru/edindex/project/algorithma/"
-
-print(url.split("/")[-2])
