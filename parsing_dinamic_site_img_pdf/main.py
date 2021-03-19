c3cb85e877f8c0b09ab40c9138e03f26484452a8
Carden <carden.ruby@gmail.com>
2021-03-19 19:52:06 +0300

    add main

---
+++ parsing_dinamic_site_img_pdf/main.py
@@ -0,0 +1,38 @@
+import requests
+import img2pdf
+
+
+def get_data():
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,"
+                  "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                      " Chrome/89.0.4389.82 Safari/537.36"
+    }
+
+    img_list = []
+    for i in range(1, 49):
+        url = f"http://recordpower.co.uk/flip/Winter2020/files/mobile/{i}.jpg"
+        req = requests.get(url=url, headers=headers)
+        response = req.content
+
+        with open(f"media/{i}.jpg", "wb") as file:
+            file.write(response)
+            img_list.append(f"media/{i}.jpg")
+            print(f"Downloaded {i} of 48")
+    print("#" * 20)
+    print(img_list)
+
+    # Конвертация в PDF
+    with open("result.pdf", "wb") as file:
+        file.write(img2pdf.convert(img_list))
+
+    print("PDF file created successfully")
+
+
+def main():
+    get_data()
+
+
+if __name__ == '__main__':
+    main()
